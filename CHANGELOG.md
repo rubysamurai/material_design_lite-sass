82ffc8146c9d6a7c0a67ddd4e839ae743d6b0bf1
Dmitriy Tarasov <info@rubysamurai.com>
2015-07-25 18:08:19 +0300

    Release v1.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 1.0.1 (2015-07-25)
+
+- Initial release
+
+Library version: Material Design Lite v1.0.1
--- lib/material_design_lite/sass/version.rb
+++ lib/material_design_lite/sass/version.rb
@@ -1,5 +1,5 @@
 module MaterialDesignLite
   module Sass
-    VERSION = '0.1.0'
+    VERSION = '1.0.1'
   end
 end
