f5e58a38231d8cfaf0e1132ce2caa62093d8441f
Dmitriy Tarasov <info@rubysamurai.com>
2015-11-21 14:17:31 +0300

    Support for sprockets-rails 3+

--- lib/material_design_lite/sass/engine.rb
+++ lib/material_design_lite/sass/engine.rb
@@ -6,9 +6,12 @@ module MaterialDesignLite
           %w(stylesheets javascripts fonts images).each do |sub|
             app.config.assets.paths << root.join('vendor/assets', sub).to_s
           end
-          app.config.assets.precompile << %r(material-icons/MaterialIcons-Regular\.(?:eot|ttf|woff|woff2?)$)
-          app.config.assets.precompile << %r(roboto/Roboto-[\w-]+\.(?:ttf|woff|woff2?)$)
-          app.config.assets.precompile += %w[*.svg]
+
+          unless Sprockets::Rails::VERSION.split('.', 2)[0].to_i >= 3
+            app.config.assets.precompile << %r(material-icons/MaterialIcons-Regular\.(?:eot|ttf|woff|woff2?)$)
+            app.config.assets.precompile << %r(roboto/Roboto-[\w-]+\.(?:ttf|woff|woff2?)$)
+            app.config.assets.precompile += %w[*.svg]
+          end
         end
       end
     end
